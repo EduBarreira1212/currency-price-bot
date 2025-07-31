5bca15daac8ad3e4bea3cc373b2909597cae35d6
Eduardo Barreira <edubarreira1212@gmail.com>
2025-07-31 18:29:57 -0300

    feat: create service to get btc price

--- internal/price/service.go
+++ internal/price/service.go
@@ -1,1 +1,29 @@
 package price
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type Service struct{}
+
+func NewService() *Service {
+	return &Service{}
+}
+
+func (s *Service) GetBitcoinPrice() (string, error) {
+	resp, err := http.Get("https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd")
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	var data map[string]map[string]float64
+	if err := json.NewDecoder(resp.Body).Decode(&data); err != nil {
+		return "", err
+	}
+
+	price := data["bitcoin"]["usd"]
+	return fmt.Sprintf("%.2f", price), nil
+}
