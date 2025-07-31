697434d25ba11fd251fbdbc57ea1e3d864f303d1
Eduardo Barreira <edubarreira1212@gmail.com>
2025-07-31 18:30:24 -0300

    feat: create handler to create and start telegram bot

--- internal/telegram/handler.go
+++ internal/telegram/handler.go
@@ -1,1 +1,44 @@
 package telegram
+
+import (
+	"currency-price-bot/internal/price"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+type Bot struct {
+	api   *tgbotapi.BotAPI
+	price *price.Service
+}
+
+func NewBot(token string, priceService *price.Service) *Bot {
+	bot, err := tgbotapi.NewBotAPI(token)
+	if err != nil {
+		panic(err)
+	}
+
+	return &Bot{api: bot, price: priceService}
+}
+
+func (b *Bot) Start() {
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+	updates := b.api.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message == nil {
+			continue
+		}
+
+		if update.Message.Text == "/btc" {
+			btcPrice, err := b.price.GetBitcoinPrice()
+			msgText := "Bitcoin price: $" + btcPrice
+			if err != nil {
+				msgText = "Failed to fetch price: " + err.Error()
+			}
+
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, msgText)
+			b.api.Send(msg)
+		}
+	}
+}
