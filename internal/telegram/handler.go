9e44cca33c06444a8d06662e7501dd69ce1761de
Eduardo Barreira <edubarreira1212@gmail.com>
2025-08-03 16:35:26 -0300

    feat: create inline buttons to get prices

--- internal/telegram/handler.go
+++ internal/telegram/handler.go
@@ -26,30 +26,55 @@ func (b *Bot) Start() {
 	updates := b.api.GetUpdatesChan(u)
 
 	for update := range updates {
-		if update.Message == nil {
+		if update.Message != nil && update.Message.Text == "/start" {
+			b.handleStartCommand(update.Message)
 			continue
 		}
 
-		if update.Message.Text == "/btc" {
-			btcPrice, err := b.price.GetBitcoinPrice()
-			msgText := "Bitcoin price: $" + btcPrice
-			if err != nil {
-				msgText = "Failed to fetch price: " + err.Error()
-			}
+		if update.CallbackQuery != nil {
+			b.handleCallback(update.CallbackQuery)
+			continue
+		}
+	}
+}
 
-			msg := tgbotapi.NewMessage(update.Message.Chat.ID, msgText)
-			b.api.Send(msg)
+func (b *Bot) handleStartCommand(msg *tgbotapi.Message) {
+	btcButton := tgbotapi.NewInlineKeyboardButtonData("💰 Get Bitcoin Price", "get_btc")
+	ethButton := tgbotapi.NewInlineKeyboardButtonData("💰 Get Ethereum Price", "get_eth")
+	keyboard := tgbotapi.NewInlineKeyboardMarkup(
+		tgbotapi.NewInlineKeyboardRow(btcButton, ethButton),
+	)
+
+	text := "Welcome! Click the button below to get the price of the choosen coin:"
+	message := tgbotapi.NewMessage(msg.Chat.ID, text)
+	message.ReplyMarkup = keyboard
+
+	b.api.Send(message)
+}
+
+func (b *Bot) handleCallback(cb *tgbotapi.CallbackQuery) {
+	switch cb.Data {
+	case "get_btc":
+		b.api.Request(tgbotapi.NewCallback(cb.ID, "Fetching BTC price..."))
+
+		price, err := b.price.GetBitcoinPrice()
+		msgText := "💵 BTC: $" + price
+		if err != nil {
+			msgText = "❌ Error fetching BTC price: " + err.Error()
 		}
 
-		if update.Message.Text == "/eth" {
-			ethPrice, err := b.price.GetEthereumPrice()
-			msgText := "Ethereum price: $" + ethPrice
-			if err != nil {
-				msgText = "Failed to fetch price: " + err.Error()
-			}
+		msg := tgbotapi.NewMessage(cb.Message.Chat.ID, msgText)
+		b.api.Send(msg)
+	case "get_eth":
+		b.api.Request(tgbotapi.NewCallback(cb.ID, "Fetching ETH price..."))
 
-			msg := tgbotapi.NewMessage(update.Message.Chat.ID, msgText)
-			b.api.Send(msg)
+		price, err := b.price.GetEthereumPrice()
+		msgText := "💵 ETH: $" + price
+		if err != nil {
+			msgText = "❌ Error fetching ETH price: " + err.Error()
 		}
+
+		msg := tgbotapi.NewMessage(cb.Message.Chat.ID, msgText)
+		b.api.Send(msg)
 	}
 }
