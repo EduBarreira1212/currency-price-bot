ee1e7b66b1e3d132bd1984d4759e9609485e3d81
Eduardo Barreira <edubarreira1212@gmail.com>
2025-08-03 16:25:13 -0300

    feat: create service ti get ethereum price

--- internal/price/service.go
+++ internal/price/service.go
@@ -27,3 +27,19 @@ func (s *Service) GetBitcoinPrice() (string, error) {
 	price := data["bitcoin"]["usd"]
 	return fmt.Sprintf("%.2f", price), nil
 }
+
+func (s *Service) GetEthereumPrice() (string, error) {
+	resp, err := http.Get("https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd")
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	var data map[string]map[string]float64
+	if err := json.NewDecoder(resp.Body).Decode(&data); err != nil {
+		return "", err
+	}
+
+	price := data["ethereum"]["usd"]
+	return fmt.Sprintf("%.2f", price), nil
+}
